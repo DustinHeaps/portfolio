d3013769311fc6651ddd5d2973edb62ee0af564a
Dustin heaps <dustinheaps89@gmail.com>
2020-01-22 21:58:56 -0500

    Updates

---
+++ precache-manifest.78a40f14912fdb8efb3049cc1ec810bc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "68cfc3c243d49e972ed3",
+    "url": "/portfolio/static/css/main.4e0aadaf.chunk.css"
+  },
+  {
+    "revision": "68cfc3c243d49e972ed3",
+    "url": "/portfolio/static/js/main.5311a7d6.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "da5a7f479f2682f4d6ce",
+    "url": "/portfolio/static/css/2.e191c1a9.chunk.css"
+  },
+  {
+    "revision": "da5a7f479f2682f4d6ce",
+    "url": "/portfolio/static/js/2.3db122a2.chunk.js"
+  },
+  {
+    "revision": "87afc57ad7095b2701a55860bd3f808d",
+    "url": "/portfolio/static/media/jungle.87afc57a.jpg"
+  },
+  {
+    "revision": "8cdcd63891bb0f808ef59ff3072f7d1c",
+    "url": "/portfolio/static/media/triviaAppBg.8cdcd638.png"
+  },
+  {
+    "revision": "67b05d8c879331f63d75810d230e1bdd",
+    "url": "/portfolio/static/media/faceFinderBg.67b05d8c.png"
+  },
+  {
+    "revision": "c11d641577e1781ad2ebfcf4b42bdad6",
+    "url": "/portfolio/static/media/hats.c11d6415.png"
+  },
+  {
+    "revision": "c4bcf87d5a37c832c92f763f324c6285",
+    "url": "/portfolio/static/media/weather.c4bcf87d.png"
+  },
+  {
+    "revision": "036d87d240058dfabbd7564f08406d8f",
+    "url": "/portfolio/static/media/Resume2020.036d87d2.pdf"
+  },
+  {
+    "revision": "f185e62fa6884b5b3fc4ddaa553b0e81",
+    "url": "/portfolio/static/media/logo-white.f185e62f.svg"
+  },
+  {
+    "revision": "2527af917ca933803bbc95b77de77aa1",
+    "url": "/portfolio/static/media/logo-green.2527af91.svg"
+  },
+  {
+    "revision": "9a071c14355b24ed10f62913d126fce3",
+    "url": "/portfolio/index.html"
+  }
+];
